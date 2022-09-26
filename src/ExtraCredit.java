4cd212a608e0af826343065e104011ddcc57f0be
khan2rs <khan2rs@mail.uc.edu>
2022-09-25 22:39:19 -0400

    Initial commit

---
+++ src/ExtraCredit.java
@@ -0,0 +1,14 @@
+public class ExtraCredit {
+    public static void main(String[] args) {
+
+        int number1 =40;
+        int number2 = 30;
+        if (number1 == number2) {
+            System.out.println("The numbers are equal");
+        }else if (number1 > number2) {
+            System.out.println("Number 1 is greater than Number 2");
+        }else if (number1 < number2){
+            System.out.println("Number 2 is greater than Number 1");
+        }
+    }
+}
